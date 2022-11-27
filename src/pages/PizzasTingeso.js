374516070dc49ba7ce3aa269a5cd31f328fa7163
darsmen32@gmail.com <darsmen32@gmail.com>
2022-11-27 17:56:40 -0300

    Initial frontend data

---
+++ src/pages/PizzasTingeso.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+const PizzasTingeso = () => {
+    return (
+        <div className="PizzasTingeso">
+            <h1>Pizzas</h1>
+        </div>
+    )
+}
+
+export default PizzasTingeso
