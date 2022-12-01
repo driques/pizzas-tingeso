27329283f75048e38fd44d14f84c3ed0bdda8d6b
darsmen32@gmail.com <darsmen32@gmail.com>
2022-12-01 15:12:33 -0300

    Few changes in navbar

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import PizzasTingeso from "./pages/PizzasTingeso";
-import {Nav} from "react-bootstrap";
-import Example from "./navigation/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PizzasApp() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<PizzasTingeso />}>
-                </Route>
-                <Route path="/test" element={<Nav />}>
-                </Route>
-                <Route path="/test2" element={<Example />}>
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/pages/PizzasTingeso.js
+++ src/pages/PizzasTingeso.js
@@ -1,12 +1,26 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import './PizzasTingeso.css';
 
-const PizzasTingeso = () => {
-    return (
-        <div className="PizzasTingeso">
-            <h1>Pizzas</h1>
-        </div>
-    )
+
+function PizzasTingeso() {
+    return (<>
+            <Navbar className="navbar-custom">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src="https://github.com/driques/pizzas-tingeso/blob/0aef1bec18e03ee01085bfacb65103dc768ceec6/src/img/logo.svg?raw=true"
+                            width="70"
+                            height="70"
+                            className="d-inline-block align-mid"
+                            alt="Pizzas Tingeso"
+                        />
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
+
+    );
 }
 
 export default PizzasTingeso
